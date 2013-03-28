61ddecb05283136fc2389533461950c12abe4a96
tpaszkowski <tpaszkowski@suse.com>
2013-03-28 11:35:51 -0400

    enable quantum

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,10 @@
 
 default["cloudbuilder"]["default_hostname"] = "linux-pdfy"
 default_hostname = default["cloudbuilder"]["default_hostname"]
+default_runlist = ["db", "mq", "keystone","glance", "nova-api", "conductor", "scheduler", "compute", "quantum-server", "quantum-openvswitch-agent", "quantum-l3-agent", "quantum-dhcp-agent"]
 
-default["cloudbuilder"]["hosts"][default_hostname]["roles"] = ["db", "mq", "keystone","glance", "nova-api", "conductor", "scheduler", "compute","quantum-server"]
-default["cloudbuilder"]["hosts"]["linux-f0kx"]["roles"] = ["db", "mq", "keystone","glance", "nova-api", "conductor", "scheduler", "compute", "quantum-server"]
+default["cloudbuilder"]["hosts"][default_hostname]["roles"] = default_runlist
+default["cloudbuilder"]["hosts"]["linux-f0kx"]["roles"] = default_runlist
 
 
 default["cloudbuilder"]["recipies"]["role"]["db"] = ["mysql::server", "keystone::db", "glance::db", "nova::db", "quantum::db"]
@@ -17,3 +18,6 @@ default["cloudbuilder"]["recipies"]["role"]["conductor"] = ["openstack-common", 
 default["cloudbuilder"]["recipies"]["role"]["scheduler"] = ["openstack-common", "nova::scheduler"]
 default["cloudbuilder"]["recipies"]["role"]["compute"] = ["openstack-common", "nova::libvirt", "nova::compute"]
 default["cloudbuilder"]["recipies"]["role"]["quantum-server"] = ["quantum::server"]
+default["cloudbuilder"]["recipies"]["role"]["quantum-l3-agent"] = ["quantum::l3-agent"]
+default["cloudbuilder"]["recipies"]["role"]["quantum-openvswitch-agent"] = ["quantum::openvswitch-agent"]
+default["cloudbuilder"]["recipies"]["role"]["quantum-dhcp-agent"] = ["quantum::dhcp-agent"]
